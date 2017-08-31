06a4951ed53a715d8b570c8b2a14e13693b2c5ba
CodeRi13 <ruto1924@gmail.com>
2017-08-31 10:26:51 +0900

    Add admin remove check logic

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -7,3 +7,13 @@ $('.item').click(function () {
         document.forms['vote'].submit();
     }
 });
+
+$('.adminitem').click(function () {
+    var isDoing = confirm('정말 이 관리자를 재명하시겠습니까?');
+    if (isDoing) {
+        var admin_id = $(this).data('adminid');
+
+        $('#inform').html('<form action="/manager/admins/delete" name="vote" method="post" style="display:none;"><input type="hidden" name="admin_id" value="' + admin_id + '"/></form>');
+        document.forms['vote'].submit();
+    }
+});
